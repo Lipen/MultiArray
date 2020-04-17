3ee336093bdf3734751c006961dcf789a4bd072d
Konstantin Chukharev <lipen00@gmail.com>
2020-04-17 21:14:58 +0300

    Add getOrDefault, getOrNull and getOrElse extensions

--- src/main/kotlin/com/github/lipen/multiarray/MultiArrayExtensions.kt
+++ src/main/kotlin/com/github/lipen/multiarray/MultiArrayExtensions.kt
@@ -1,4 +1,18 @@
+@file:Suppress("FunctionName")
+
 package com.github.lipen.multiarray
+
+fun <T> MultiArray<T>.getOrDefault(vararg index: Int, defaultValue: T): T = getOrDefault_(index, defaultValue)
+fun <T> MultiArray<T>.getOrDefault_(index: IntArray, defaultValue: T): T =
+    if (index in indices) getAt(index) else defaultValue
+
+fun <T> MultiArray<T>.getOrNull(vararg index: Int): T? = getOrNull_(index)
+fun <T> MultiArray<T>.getOrNull_(index: IntArray): T? =
+    if (index in indices) getAt(index) else null
+
+fun <T> MultiArray<T>.getOrElse(vararg index: Int, defaultValue: () -> T): T = getOrElse_(index, defaultValue)
+fun <T> MultiArray<T>.getOrElse_(index: IntArray, defaultValue: () -> T): T =
+    if (index in indices) getAt(index) else defaultValue()
 
 inline fun <T> MultiArray<T>.fillBy(init: (IntArray) -> T) {
     for (index in indices)
